5bc73a919e70546273fc7a68ed3f24ac85dbfbeb
Kağan <kagan-bayar1@hotmail.com>
2026-04-12 22:06:07 +0300

    update: prisma updated

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -1,15 +1,15 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaPostgresAdapter } from "@prisma/adapter-ppg";
+const adapter = new PrismaPostgresAdapter({
+  connectionString: process.env.DATABASE_URL!,
+});
 
-const prismaClientSingleton = () => {
-  return new PrismaClient();
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient;
 };
 
-declare const globalThis: {
-  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
-} & typeof global;
-
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
 
 export default prisma;
 
-if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
